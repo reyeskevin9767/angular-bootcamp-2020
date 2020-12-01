f90da1f50465f4958f1b11b5fb073507ac535cf2
Kevin Reyes <reyeskevin9767@gmail.com>
2020-11-30 23:41:32 -0500

    create password generated function

--- 01-first-project/passwordGenerator/src/app/app.component.ts
+++ 01-first-project/passwordGenerator/src/app/app.component.ts
@@ -31,6 +31,32 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = 'My PASSWRORD';
+    const numbers = '1234567890';
+    const letters = 'abcedefghiklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    // New Password
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
