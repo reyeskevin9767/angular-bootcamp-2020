568e636e6d64508bb1693de9787ddda1077c6416
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-07 08:43:26 -0500

    if and for loops into html

--- 03-third-project/typing/src/app/app.component.ts
+++ 03-third-project/typing/src/app/app.component.ts
@@ -7,5 +7,18 @@ import { lorem } from 'faker';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
