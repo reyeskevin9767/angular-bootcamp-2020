c1ecf6bee566fc4fb450905a1b1d46b09006f810
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-11 01:54:23 -0500

    add new pipes

--- 04-fourth-project/pipes/src/app/app.component.ts
+++ 04-fourth-project/pipes/src/app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  date: string;
+  amount: number;
+  height: number;
 
   onNameChange(value: string) {
     this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
   }
 }
