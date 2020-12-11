9a968ac4eaa8b8d8acdeb58ff2a79a7406fccbf8
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-11 02:14:26 -0500

    add convert pipe

--- 04-fourth-project/pipes/src/app/convert.pipe.ts
+++ 04-fourth-project/pipes/src/app/convert.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'convert'
+  name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-
   transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (!value) {
+      return '';
+    }
+    return value * 1.60934;
   }
-
 }
