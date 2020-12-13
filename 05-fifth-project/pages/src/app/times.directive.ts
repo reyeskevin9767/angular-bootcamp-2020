b221bc433aed4f32fe62c631edeb80a7e5831dad
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-12 23:25:55 -0500

    create for directive

--- 05-fifth-project/pages/src/app/times.directive.ts
+++ 05-fifth-project/pages/src/app/times.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appTimes]'
+  selector: '[appTimes]',
 })
 export class TimesDirective {
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) {}
 
-  constructor() { }
+  @Input('appTimes') set render(times: number) {
+    this.viewContainer.clear();
 
+    for (let i = 0; i < times; i++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, {});
+    }
+  }
 }
