a415ea4cf96898297999ac728e3e42cfe97081e2
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-12 23:16:47 -0500

    add new directive

--- 05-fifth-project/pages/src/app/class.directive.ts
+++ 05-fifth-project/pages/src/app/class.directive.ts
@@ -7,7 +7,13 @@ export class ClassDirective {
   constructor(private element: ElementRef) {}
 
   // Make a directive that responds to a property set on it
-  @Input('appClass') set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
