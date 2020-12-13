f687c5b670c98cb362e3328d56f908f5a4a1a27b
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-12 23:10:01 -0500

    reasign input

--- 05-fifth-project/pages/src/app/class.directive.ts
+++ 05-fifth-project/pages/src/app/class.directive.ts
@@ -7,7 +7,7 @@ export class ClassDirective {
   constructor(private element: ElementRef) {}
 
   // Make a directive that responds to a property set on it
-  @Input() set appClass(color: string) {
+  @Input('appClass') set backgroundColor(color: string) {
     this.element.nativeElement.style.backgroundColor = color;
   }
 }
