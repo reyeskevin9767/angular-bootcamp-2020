77a18bb43c7661090bafaad066e81882dd46decf
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-16 22:15:22 -0500

    fix routing

--- 06-sixth-project/comps/src/app/app.module.ts
+++ 06-sixth-project/comps/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
@@ -10,7 +9,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
